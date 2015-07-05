4db6ddbaae3a4f4ab6147bf1f82fade8a15e1e76
seabag <x72xakwmxoxo@gmail.com>
2015-07-05 13:29:40 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sns2App::Application.config.secret_key_base = '968885eee7f0d01104bd63487f8504518cf677564de2d6afe3f50f156321315df07d93c69d3a67d04f162bb7644ea8db102578dfdb74ea76beda323e9c99f17f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sns2App::Application.config.session_store :cookie_store, key: '_sns2_app_session'
